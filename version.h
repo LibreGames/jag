8dfd1f958f0aace3d7337a96f020c41c13d7a7eb
Rémi Verschelde <rverschelde@gmail.com>
2016-10-08 22:05:02 +0200

    Version 0.3.3
    Adds Qt5 support and some Linux packaging improvements.

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GAME_VERSION        0x000302
-#define GAME_VERSION_STR    "0.3.2"
+#define GAME_VERSION        0x000303
+#define GAME_VERSION_STR    "0.3.3"
 
 #endif // VERSION_H
